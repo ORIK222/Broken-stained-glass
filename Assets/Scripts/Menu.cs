8e8cc28acdee09cb9106c1ae6e4302c0232d116e
TaniaAAAAAAA <tsntania@gmail.com>
2020-11-30 17:38:46 +0200

    "Додала сцену з меню"

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+
+//public class Menu : MonoBehaviour
+//{
+//    public void StartButtonOnClick()
+//    {
+//       // SceneManager.Load("TestScene");
+//    }
+//}
