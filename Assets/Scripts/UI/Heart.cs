0c220eb4b4213ba0f8efb44d66ce63a44a134018
TaniaAAAAAAA <tsntania@gmail.com>
2020-12-02 13:13:50 +0200

    "Механіка сердечок"

---
+++ Assets/Scripts/UI/Heart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour
+{
+    public bool IsLost = false;
+    [SerializeField] private Sprite _sprite;
+
+
+    private void Start()
+    {
+        _sprite = GetComponent<Image>().sprite;
+
+    }
+}
+
+
---
+++ Assets/Scripts/UI/HeartControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartControler : MonoBehaviour
+{
+    [SerializeField] private Heart[] _hearts;
+    [SerializeField] private Sprite _darkHeart;
+
+    private void Start()
+    {
+        _hearts = new Heart[transform.childCount];
+        HeartsInit();
+    }
+
+    private void HeartsInit()
+    {
+        for(int i=0;i<_hearts.Length;i++)
+        {
+            _hearts[i] = transform.GetChild(i).GetComponent<Heart>();
+        }
+    }
+
+    public void ChangeHeartsState()
+    {
+        for(int i=0;i<_hearts.Length;i++)
+        {
+            if (_hearts[i].IsLost == false)
+            {
+                _hearts[i].IsLost = true;
+                _hearts[i].GetComponent<Image>().sprite = _darkHeart;
+                break;
+            }
+            else continue;
+
+        }
+    }
+
+}
